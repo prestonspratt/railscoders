e99a89544f49891abf5ade1ae1153eabd60ee5ab
Preston Spratt <prestonspratt@gmail.com>
2012-12-13 15:11:33 -0500

    Fixed Permalink and page create
    Worked up to page 64 of the book.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,15 +1,23 @@
 class PagesController < ApplicationController
   def index
-  @pages = Page.find(:all)
+    @pages = Page.find(:all)
   end
 
   def show
+    @page = Page.find(params[:id].to_i)
   end
 
   def new
+    @page = Page.new
   end
 
   def create
+    @page = Page.new(params[:page])
+    @page.save!
+    flash[:notice] = 'Page saved'
+    redirect_to :action => 'index'
+    rescue ActiveRecord::RecordInvalid
+    render :action => 'new'
   end
 
   def edit
--- app/models/page.rb
+++ app/models/page.rb
@@ -3,8 +3,14 @@ class Page < ActiveRecord::Base
   validates_length_of :title, :within => 3..255
   validates_length_of :body, :maximum => 10000
   
-  def before_create
+  before_create :create_permalink
+  
+  def create_permalink
     @attributes['permalink'] =
     title.downcase.gsub(/\s+/, '_').gsub(/[^a-zA-Z0-9_]+/, '')
+  end
+  
+  def to_param
+    "#{id}-#{permalink}"
   end
 end
